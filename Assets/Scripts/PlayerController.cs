28ff944442fea89c5d0ad1e69891bede100cce70
Future Deus <FutureDeus@live.hk>
2020-04-03 16:30:48 -0700

    Assignment 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private int jump_count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+
+        if (Input.GetKeyDown("space") && jump_count < 2){
+            jump_count++;
+            Vector3 jump = new Vector3(0.0f, 20.0f, 0.0f);
+            rb.AddForce (jump * speed);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void OnCollisionEnter(){
+        jump_count = 0;
+    }
+
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 8){
+            winText.text = "You Win!";
+        }
+    }
+}
